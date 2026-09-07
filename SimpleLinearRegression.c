6589f00eb1e82562b76d9eb4810fa41e9637a372
Chandrangshu Dey <135465899+r1shi13@users.noreply.github.com>
2026-09-07 20:16:37 +0530

    Simple Linear Regression using custom Matrix struct

---
+++ SimpleLinearRegression.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct {
+    float *data;
+    int rows;
+    int cols;
+} Matrix;
+
+int main(){
+    FILE *p=fopen("Salary.csv","r");
+    char b1[255];
+    char b2[255];
+
+    int i=0;    
+    
+    if (fgets(b1, 255, p) != NULL) {
+        printf("%s", b1);
+    }
+    int c=0;
+    for (int i1=0;i1<255 && b1[i1]!='\0';i1++){
+        if (b1[i1]==','){
+            c++;
+        }
+    }
+    Matrix m;
+    m.rows=0;
+    m.cols=c+1;
+    m.data=(float*)malloc(sizeof(float)*m.cols);
+    if (m.data == NULL) return 1;
+    while(fgets(b2,255,p)!=NULL){
+        m.data[i]=strtof(strtok(b2,","),NULL);
+        for(int a=1;a<m.cols;a++){
+            m.data[i+a]=strtof(strtok(NULL,","),NULL);     
+        }
+        m.rows++;
+        i=m.rows*m.cols;
+        float *temp=(float*)realloc(m.data,sizeof(float)*(m.cols+i));
+        if(temp!=NULL){
+            m.data=temp;
+        }
+
+    }
+    fclose(p);
+    double xs=0,ys=0,xys=0,x2s=0;
+
+    for (int i1=1;i1<i;i1+=3){
+        xs+=m.data[i1];
+        x2s+=(m.data[i1])*(m.data[i1]);
+        xys+=(m.data[i1])*(m.data[i1+1]);
+        ys+=m.data[i1+1];
+
+    }
+    printf("%f,%f,%f,%f",xs,ys,x2s,xys);
+
+    double beta1=(xys-(xs*ys/m.rows))/(x2s-((xs)*(xs)/m.rows));
+
+    printf("\n%f",beta1);
+
+    double beta0 = (ys/m.rows)-(beta1*(xs/m.rows));
+
+    printf("\nThe regression line is y=%.2f+%.2fx.",beta0,beta1);
+
+    free(m.data);
+
+}
